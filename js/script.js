56925a4d133e3bf7300aee855aeba9550a5eb369
GianmarcoBramucci <giangian797@gmail.com>
2023-09-13 00:10:26 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@ const { createApp } = Vue
                         },
             methods:{
                 genereteMail(){
-                    axios
-                        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                        .then((response) => {
-                            console.log(response.data.response);
-                        });
+                    for(let i=0;i<10;i++){
+                        axios
+                            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                            .then((response) => {
+                                this.emails.push(response.data.response);
+                            });
+                        console.log(this.emails[i]);
+                    }
                 },
             },
                 mounted(){
