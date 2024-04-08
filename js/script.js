d50e51be0902c97432dbc52231355e650eb6befa
GianmarcoBramucci <giangian797@gmail.com>
2024-04-08 17:57:09 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -3,14 +3,30 @@
 Vue.createApp({
     data(){
         return{
-    
+            emails: [],
+            numEamil : 10,
+            counteinerEmail: []
         }
     },
     methods: {
+        getMails : function(){
+            this.counteinerEmail =[]; 
+            this.emails =[];   
+            for(let i=0;i<(this.numEamil);i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((ris)=>{
+                        this.emails.push(ris.data.response);               
+                        if(this.emails.length == this.numEamil){
+                            this.counteinerEmail =this.emails;
+                        }
+                    })
+                }
+                
+        }     
     },
     computed:{
+
     },
     mounted(){
-       
+        this.getMails();
     }
 }).mount('#app')
